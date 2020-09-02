374a9e27c3884ec9e544590f1b40cb45bd8876a0
pasqualespica <pasqualespica@gmail.com>
2020-09-02 18:07:22 +0200

    upd hmac to create specific container

--- hmac_ex.js
+++ hmac_ex.js
@@ -1,12 +1,18 @@
-crypto = require('crypto');
+const crypto = require('crypto');
+const fs = require('fs');
+const CsvReadableStream = require('csv-reader');
+const uuidv1 = require('uuid');
+const ObjectsToCsv = require('objects-to-csv');
 
-let sharedSecret = 'my-super-secret-pwd1';
-let myMessage = 'psp#identifcativo-nome-del-psp#idnumero';
+// CSV reader
+let inputStream = fs.createReadStream('resources/pagopa-psp.csv', 'utf8');
+const sharedSecret = 'my-super-secret-pwd1';
+const myMessage = 'abc1234566778';
+var csvData = [];
 
+// 1. HMAC function
 function myEncrypt(myString2Encrypt, sharedSecret) {
-  let retrievedSignature, computedSignature;
-
-  computedSignature = crypto
+  const computedSignature = crypto
     .createHmac('sha256', sharedSecret)
     .update(myString2Encrypt)
     .digest('hex');
@@ -14,9 +20,52 @@ function myEncrypt(myString2Encrypt, sharedSecret) {
   return computedSignature;
 }
 
-console.log(
-  `Message: ${myMessage}\nwith: ${sharedSecret}\ncomputed signature: ${myEncrypt(
-    myMessage,
-    sharedSecret
-  )} `
-);
+let countPsp = 0;
+// 2. HMAC function
+inputStream
+  .pipe(
+    new CsvReadableStream({
+      parseNumbers: true,
+      parseBooleans: true,
+      trim: true,
+      skipHeader: true,
+    })
+  )
+  .on('data', function (row) {
+    const psp = JSON.stringify(row).split(';');
+    console.log(`${++countPsp}) ${psp[0].substring(2)} :`);
+    const codiceFiscale = psp[1];
+    const codiceABI = psp[2];
+    let secretPwd = psp[8];
+    let containerName_ = psp[9];
+    // Create a unique name for the container
+    let my_uuidv1 = uuidv1.v4(); // Create a version 4 (random) UUID
+
+    secretPwd = secretPwd ? secretPwd : my_uuidv1;
+
+    const secretPartContainerName = myEncrypt(codiceFiscale, secretPwd);
+    const containerName = containerName_
+      ? containerName_
+      : `cont${codiceABI}${secretPartContainerName}`;
+
+    console.log(
+      `\tsecret: ${my_uuidv1}\n\tcomputed container name: ${containerName} `
+    );
+
+    if (secretPwd) {
+      csvData.push(`${row};${secretPwd};${containerName}`);
+    } else {
+      csvData.push(`${row}`);
+    }
+  })
+  .on('end', function (data) {
+    fs.writeFileSync('resources/pagopa-psp-upd.csv', csvData.join('\n'));
+    console.log('File written successfully\n');
+  });
+
+// console.log(
+//   `Message: ${myMessage}\nwith: ${sharedSecret}\ncomputed signature: ${myEncrypt(
+//     myMessage,
+//     sharedSecret
+//   )} `
+// );
